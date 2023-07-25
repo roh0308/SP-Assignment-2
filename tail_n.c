ef01b529c80c16d7ea2e42c41b2ada15ae6dfed7
Rohit Gupta <rohitgupta03081996@gmail.com>
2023-07-25 15:59:17 +0530

    hi

--- tail_n.c
+++ tail_n.c
@@ -1,5 +1,65 @@
 #include<stdio.h>
-int main()
+#include<stdlib.h>
+#define MAX_SIZE 1000
+struct line{
+    char* data;
+    struct line* next;
+};
+struct line* head=NULL;
+struct line* tail=NULL;
+
+int n=10;
+void push(char* data){
+    struct line* newline=malloc(sizeof(struct line));
+    newline->data=data;
+    newline->next=NULL;
+    if(head==NULL){
+        head=newline;
+        tail=newline;
+    }
+    else
+    {
+        tail->next=newline;
+        tail=newline;
+    }
+}
+void pop() {
+  if(head==NULL){
+    return;
+  }
+  struct line *prevhead=head;
+  head=head->next;
+  if(head==NULL){
+    tail = NULL;
+  }
+  free(prevhead->data);
+  free(prevhead);
+}
+void printlast_n(){
+  struct line *current=head;
+  int i;
+  for(i=0;i<n;i++){
+    if(current==NULL){
+      break;
+    }
+    printf("%s\n", current->data);
+    current=current->next;
+  }
+}
+
+int main(int argc,char *argv[])
 {
-    printf("Hello");
+    if(argc>2)
+    {
+        printf("Usage: tail [-n <number of lines>]\n");
+        return 1;
+    }
+    if(argc==2){
+        n=atoi(argv[1]);
+    }
+    char line[MAX_SIZE];
+    while(fgets(line,MAX_SIZE,stdin)){
+        push(line);
+    }
+    printlast_n();
 }
